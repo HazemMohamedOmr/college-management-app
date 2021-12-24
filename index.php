63f49fc84a1a12ed3fe5e37c78999e4bf1f58fda
HazemMohamedOmr <55815459+HazemMohamedOmr@users.noreply.github.com>
2021-12-24 18:08:41 +0200

    baseProject

---
+++ index.php
@@ -0,0 +1,5 @@
+<a href="views/admin/blank.php">admin</a>
+<a href="views/superadmin/blank.php">superadmin</a>
+<a href="views/teacher/blank.php">teacher</a>
+<a href="views/student/blank.php">student</a>
+<a href="views/librarain/blank.php">librarian</a>
