b7ab63b6560b644fe3880a796cc959f8388f33cc
HazemMohamedOmr <55815459+HazemMohamedOmr@users.noreply.github.com>
2021-12-26 13:02:01 +0200

    fix home admin

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<a href="views/admin/blank.php">admin</a>
+<a href="views/admin/home.php">admin</a>
 <a href="views/superadmin/home.php">superadmin</a>
 <a href="views/teacher/blank.php">teacher</a>
 <a href="views/student/blank.php">student</a>
